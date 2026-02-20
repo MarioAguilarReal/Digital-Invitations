2fe236a99c4687d6d9684a55fe5accc2f85a5066
Mario Aguilar Real <94491813+MarioAguilarReal@users.noreply.github.com>
2026-02-19 20:01:58 -0800

    Users/mario/hands on demonstrate (#1)
    * Adding sometings
    
    * adding jesus wedding component
    
    * adding jesus wedding component
    
    ---------
    
    Co-authored-by: Mario Aguilar Real <mario.real@uabc.edu.mc>

--- resources/js/templates/registry.ts
+++ resources/js/templates/registry.ts
@@ -1,5 +1,7 @@
-import GradModern01 from "./grad_modern_01/Invitation";
+import GradStory01 from "./grad_modern_01/Invitation";
+import JesusWedding from "./jesus_wedding/Invitation";
 
 export const templateRegistry: Record<string, React.ComponentType<any>> = {
-  grad_modern_01: GradModern01,
+  grad_modern_01: GradStory01,
+  grad_minimal_02: JesusWedding,
 };  
